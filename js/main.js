65c6a9f2415d18686717c9dd0fb9428fe9cd77e0
jaouad ballat <jaouad.ballat@gmail.com>
2017-05-19 23:53:24 +0100

    random quote machine build with javascrip

---
+++ js/main.js
@@ -0,0 +1,37 @@
+var quoteText;
+var author;
+function getQuote(){
+    $.ajax({
+    url: "http://api.forismatic.com/api/1.0/",
+    jsonp:'jsonp',
+    data:{
+      method: 'getQuote',
+      lang: 'en',
+      format: 'jsonp'
+    },
+    dataType:'jsonp',
+    method:'GET',
+    success:function(data){
+       quoteText = data.quoteText;
+       author = data.quoteAuthor;
+       $('.white-text').text(quoteText);
+       if(author){
+         $("#author").text("--"+author);
+       }else{
+         $("#author").text("-- Unknown");
+       }
+    },
+    error:function(err){
+      aler(err);
+      console.log(err);
+    }
+  });
+}  
+$(document).ready(function(){
+  $("#newQuote").on('click', function(e){
+  getQuote();
+});
+  $("#tweet").on('click', function(){
+     window.open('https://twitter.com/intent/tweet?text='+quoteText); 
+  });
+  });
